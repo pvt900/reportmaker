f9fce5425c1a7d44c6af7649183c7811a39a067f
Robert Farmer <FarmerRobertA@JohnDeere.com>
2019-12-03 13:37:32 -0600

    Wipes Worktracker

---
+++ WorktrackerWiper.ps1
@@ -0,0 +1,14 @@
+﻿Connect-PnPOnline -Url "http://share-internal.deere.com/teams/IEsuptJDWW/Components"
+# Put Core URL Above
+# Put List Name Below (In both Lines of the script)
+#Will it works at ~1 Item per Second the Rough Estimate is: Total Items / 60 
+$items = Get-PnPListItem -List "Worktracker"
+
+foreach ($Item in $items){
+    try{
+        Remove-PnPListItem -List "Worktracker" -Identity $Item.Id -Force
+        }
+    catch{
+        Write-Host "Error"
+        }
+}
